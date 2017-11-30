3e01647e24e70c3d875fa85fa813a6da10bdc1b7
Dylan <dquesada186@gmail.com>
2017-11-30 10:39:09 -0600

    added functional, but not pretty main function

--- happyNumbers.js
+++ happyNumbers.js
@@ -10,36 +10,92 @@ function userInput(message){
 }
 
 
-function numberToArray(number){
-	var numberArray = [];
-	for (var i = 0; i < number.length; i++) {
+function numberToArray(input){
+	let numberArray = [];
+	let number = input.toString();
+	for (let i = 0; i < number.length; i++) {
 		numberArray[i] = number.charAt(i);
 	}
 	return numberArray;
 }
 
 function squareArrayElements(array){
-	var output = [];
-	for (var i = 0; i < array.length; i++) {
+	let output = [];
+	for (let i = 0; i < array.length; i++) {
 		output[i] = array[i] * array[i];
 	}
 	return output;
 }
 
-function concatonateArrayToNumber(array){
-	var outputString = "";
+
+function addArrayElements(array){
+	let outputNumber = 0;
 	for (var i = 0; i < array.length; i++) {
-		outputString = outputString.concat(array[i])
+		outputNumber = outputNumber + parseInt(array[i]);
+	}
+	return parseInt(outputNumber);
+}
+function concatonateArrayToNumber(array){
+	let outputString = "";
+	for (let i = 0; i < array.length; i++) {
+		outputString = outputString.concat(array[i]);
 	}
 	return parseInt(outputString);
 }
-var x = [1,2,1];
-console.log(squareArrayElements(x));
-console.log(concatonateArrayToNumber(x));
-console.log(concatonateArrayToNumber(squareArrayElements(x)));
-// alert(numberToArray(userInput("Enter number:")));
-// function testHappy(number){
-// 	var 
+
+// function runIsHappyNumber(){
+// 	var end = userInput("Enter a positive integer:");
+// 	var number = end;
+// 	var value = 0;
+// 	for (var i = 0; i < 10; i++) {
+// 		if(value === end){
+// 			console.log("The number is happy.");
+// 			return true;
+// 		}
+// 		else{
+// 			value = numberToArray()
+// 		}
+// 	}
+// }
+
+function numberToString(number){
+	let output = "number";
+}
+
+function runIsHappy(){
+	let value = userInput("Enter a positive integer:");
+	var display = value;
+	let testArray = [];
+	for (let i = 0; i < 1000; i++) {
+		value = numberToArray(value);
+		value = squareArrayElements(value);
+		value = addArrayElements(value);
+		console.log(value);
+		for (var j = 0; j < testArray.length; j++) {
+			if(testArray[j] === value){
+				alert(display + " is sad.");
+				return false;
+			}
+		}
+		testArray[i] = value;
+		if(value === 1){
+			alert(display + " is a happy number!");
+			return true;
+		}
+	}
+	alert(display + " is probably sad.");
+	return false;
+}
+
+runIsHappy();
+
+// var x = [1,2,1];
+// console.log(squareArrayElements(x));
+// console.log(concatonateArrayToNumber(x));
+// console.log(concatonateArrayToNumber(squareArrayElements(x)));
+// // alert(numberToArray(userInput("Enter number:")));
+// // function testHappy(number){
+// // 	var 
 
 // }
 
